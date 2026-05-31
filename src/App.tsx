e3e4ad7cc039e5d1b4a68c215ead12d4bece7322
arthur94a <arthuraraujo94@hotmail.com>
2026-05-31 14:57:35 -0300

    create global style

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import { Home } from './pages/Home'
 import { Services } from './pages/Services'
 import { Prices } from './pages/Prices'
 import { NotFound } from './pages/NotFound'
+import { DefaultLayout } from './layout/DefaultLayout'
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route index element={<Home />} />
-                <Route path="servers" element={<Services />} />
-                <Route path="prices" element={<Prices />} />
-                <Route path="*" element={<NotFound />} />
+                <Route element={<DefaultLayout />}>
+                    <Route index element={<Home />} />
+                    <Route path="servers" element={<Services />} />
+                    <Route path="prices" element={<Prices />} />
+                    <Route path="*" element={<NotFound />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     )
