2ca8247f791973c85421db9a3850d407e26013ec
arthur94a <arthuraraujo94@hotmail.com>
2026-05-31 16:58:02 -0300

    Add new fonts

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,18 @@
 import { Button } from '../button'
+import { GiAngelWings } from 'react-icons/gi'
+
+import styles from './header.module.scss'
 
 export function Header() {
     return (
         <header>
-            <div>Ícaro Services</div>
+            <div className={styles.logo}>
+                <i className={styles.logo_icon}>
+                    <GiAngelWings stroke="black" fill="black" size={'100%'} />
+                </i>
+                <span className={styles.logo_title}>Ícaro Services</span>
+            </div>
+
             <nav>
                 <ul>
                     <li>Home</li>
