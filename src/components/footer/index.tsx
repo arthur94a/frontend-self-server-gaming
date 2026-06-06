a17c8f5266e3670c899e29f0a7beb33263f72612
arthur94a <arthuraraujo94@hotmail.com>
2026-06-06 17:42:47 -0300

    Footer style

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,9 +1,11 @@
 import { Section } from '@components/section'
 
+import styles from './footer.module.scss'
+
 export function Footer() {
     return (
-        <Section>
-            <footer>
+        <Section className={styles.section}>
+            <footer className={styles.footer}>
                 <span>© 2026 NexusHost · Game Servers</span>
                 <span>Latência baixa · Uptime 99.9% · DDoS Protection</span>
             </footer>
