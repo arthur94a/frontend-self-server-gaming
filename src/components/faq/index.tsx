443c74903b08f799e308e35a9ee744097a3e679b
Arthur <arthuraraujo94@hotmail.com>
2026-06-11 21:46:39 -0300

    Styled faq

--- src/components/faq/index.tsx
+++ src/components/faq/index.tsx
@@ -1,4 +1,9 @@
 import { Section } from '@components/section'
+import { FaPlus } from 'react-icons/fa'
+import clsx from 'clsx'
+
+import styles from './faq.module.scss'
+import { useState } from 'react'
 
 export type DetailType = {
     summary: string
@@ -6,20 +11,53 @@ export type DetailType = {
 }
 
 export function Faq({ details }: { details: DetailType[] }) {
+    const [activeIndex, setActiveIndex] = useState<number | null>(null)
+
+    const handleToggleFaq = (index: number) => {
+        setActiveIndex((current) => (current === index ? null : index))
+    }
+
     return (
-        <Section center={true}>
-            <h2>Perguntas frequentes</h2>
+        <Section center={true} className={styles.section}>
+            <h2 className={styles.title}>Perguntas frequentes</h2>
 
-            <div>
-                {details.map((detail) => (
-                    <details key={detail.summary}>
-                        <summary>{detail.summary}</summary>
+            <div className={styles.faq_container}>
+                {details.map((detail, index) => {
+                    const isOpen = activeIndex === index
 
-                        <div>
-                            <p>{detail.text}</p>
-                        </div>
-                    </details>
-                ))}
+                    return (
+                        <details
+                            key={detail.summary}
+                            className={styles.faq_box}
+                            open={isOpen}
+                            onClick={() => {
+                                handleToggleFaq(index)
+                            }}
+                        >
+                            <summary
+                                className={styles.faq_summary}
+                                onClick={(event) => {
+                                    event.preventDefault()
+                                }}
+                                aria-expanded={isOpen}
+                            >
+                                {detail.summary}
+
+                                <FaPlus
+                                    fill="#02f77b"
+                                    className={clsx(
+                                        styles.icon_plus,
+                                        isOpen && styles.rotate,
+                                    )}
+                                />
+                            </summary>
+
+                            <div className={styles.faq_description}>
+                                <p>{detail.text}</p>
+                            </div>
+                        </details>
+                    )
+                })}
             </div>
         </Section>
     )
